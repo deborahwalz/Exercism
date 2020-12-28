1b286a97ed6444a6ec37084025269b8f0ecab1ad
Deborah Walz <deborah.walz@gmx.de>
2020-12-28 15:59:07 +0100

    perfect_numbers.py

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,15 @@
 def classify(number):
-    pass
+    """
+    Determine if a number is perfect, abundant or deficient.
+    """
+    if number <= 0:
+        raise ValueError("Number is not a positive integer!")
+
+    aliquot_sum = sum([i for i in range(1, number) if (number % i == 0)])
+    
+    if aliquot_sum == number:
+        return "perfect"
+    elif aliquot_sum > number:
+        return "abundant"
+    elif aliquot_sum < number:
+       return "deficient"
