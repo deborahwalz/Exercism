09fcf0331205432384ca66336d814f7e32cf7835
Deborah Walz <deborah.walz@gmx.de>
2021-01-30 10:32:34 +0100

    anagram.py updated

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,8 +1,5 @@
 def find_anagrams(word, candidates):
-    word_counts = {s: word.lower().count(s) for s in set(word.lower())}
-    candidates = [cand for cand in candidates if cand.lower() != word.lower()]
-    anagrams = [
+    return [
         cand for cand in candidates
-        if {s: cand.lower().count(s) for s in set(cand.lower())} == word_counts
+        if sorted(cand.lower()) == sorted(word.lower()) and cand.lower() != word.lower() 
     ]
-    return anagrams
