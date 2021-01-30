a3ead484953fdc5f59ef4549fed5c9b6399a8861
Deborah Walz <deborah.walz@gmx.de>
2021-01-30 16:08:00 +0100

    new exercises

---
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,12 @@
+def steps(number):
+    if number < 1:
+        raise ValueError("Invalid!")
+
+    steps = 0
+    while number != 1:
+        steps += 1
+        if number % 2 == 0:
+            number = number / 2
+        else:
+            number = 3 * number + 1
+    return steps
