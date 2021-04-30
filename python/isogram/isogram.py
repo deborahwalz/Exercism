7dc41df5dfa082f522b8290e4b15e8b0a65be670
Deborah Walz <deborah.walz@gmx.de>
2021-04-30 18:25:04 +0200

    isogram.py

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,3 +1,3 @@
 def is_isogram(string):
-    word_cleared = string.replace(" ", "").replace("-", "").lower()
+    word_cleared = [s.lower() for s in string if s.isalpha()]
     return len(word_cleared) == len(set(word_cleared))
